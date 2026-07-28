b7f7aa43571058c0dbc250d0a971c953877e568b
allanbridel2004-hue <allanbridel2004@gmail.com>
2026-07-28 18:51:20 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const noBtn = document.getElementById("noBtn");
+
+noBtn.addEventListener("mouseover",()=>{
+
+const x=Math.random()*(window.innerWidth-150);
+
+const y=Math.random()*(window.innerHeight-80);
+
+noBtn.style.position="absolute";
+
+noBtn.style.left=x+"px";
+
+noBtn.style.top=y+"px";
+
+});
+
+document.getElementById("yesBtn").addEventListener("click",()=>{
+
+alert("❤️ Merci Patricia ❤️");
+
+});
